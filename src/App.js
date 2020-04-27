0435a5373f322601c0fa151ac5181bae3d60ca01
rdixon555 <ryan555@sky.com>
2020-04-27 10:54:14 +0100

    filtering

--- src/App.js
+++ src/App.js
@@ -1,50 +1,63 @@
-import React, { Component } from "react"
-import logo from "./logo.svg"
-import "./App.css"
-
-class LambdaDemo extends Component {
-  constructor(props) {
-    super(props)
-    this.state = { loading: false, msg: null }
-  }
-
-  handleClick = api => e => {
-    e.preventDefault()
-
-    this.setState({ loading: true })
-    fetch("/.netlify/functions/" + api)
-      .then(response => response.json())
-      .then(json => this.setState({ loading: false, msg: json.msg }))
-  }
-
-  render() {
-    const { loading, msg } = this.state
-
-    return (
-      <p>
-        <button onClick={this.handleClick("hello")}>{loading ? "Loading..." : "Call Lambda"}</button>
-        <button onClick={this.handleClick("async-dadjoke")}>{loading ? "Loading..." : "Call Async Lambda"}</button>
-        <br />
-        <span>{msg}</span>
-      </p>
-    )
-  }
-}
+import React, { Component } from 'react';
+import './App.css';
+import Information from './info-json';
 
 class App extends Component {
-  render() {
+
+  constructor(){
+    super();
+
+    this.state={
+      search:null
+    };
+  }
+
+  searchSpace=(event)=>{
+    let keyword = event.target.value;
+    this.setState({search:keyword})
+  }
+
+  render(){
+    const styleInfo = {
+      paddingRight:'10px'
+    }
+    const elementStyle ={
+      border:'solid',
+      borderRadius:'10px',
+      position:'relative',
+      left:'10vh',
+      height:'3vh',
+      width:'20vh',
+      marginTop:'5vh',
+      marginBottom:'10vh'
+    }
+    const items = Information.filter((data)=>{
+      if(this.state.search == null)
+          return data
+      else if(data.name.toLowerCase().includes(this.state.search.toLowerCase()) || data.country.toLowerCase().includes(this.state.search.toLowerCase())){
+          return data
+      }
+    }).map(data=>{
+      return(
+      <div>
+        <ul>
+          <li style={{position:'relative',left:'10vh'}}>
+            <span style={styleInfo}>{data.name}</span>
+            <span style={styleInfo}>{data.age}</span>
+            <span style={styleInfo}>{data.country}</span>
+          </li>
+        </ul>
+      </div>
+      )
+    })
+
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <LambdaDemo />
-        </header>
+      <div>
+      <input type="text" placeholder="Enter item to be searched" style={elementStyle} onChange={(e)=>this.searchSpace(e)} />
+      {items}
       </div>
     )
   }
 }
 
-export default App
+export default App;
