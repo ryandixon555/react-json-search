6976c644e8f700465f36c8dd6f2f4285b6ed2855
rdixon555 <ryan555@sky.com>
2020-04-27 13:33:02 +0100

    cleanup

--- src/App.js
+++ src/App.js
@@ -1,45 +1,105 @@
-import React, { useState, useEffect } from 'react';
-import './App.css';
+import React, { useState, useEffect } from "react";
 
-import Information from './info-json';
+const data = [
+  {
+    id: "1",
+    name: "rex",
+    type: "dog",
+    emoji: "🐶",
+    keywords: ["dog", "friendly", "inteligent"]
+  },
+  {
+    id: "2",
+    name: "garfield",
+    type: "cat",
+    emoji: "🐱",
+    keywords: ["cat", "independent"]
+  },
+  {
+    id: "3",
+    name: "foxy",
+    type: "fox",
+    emoji: "🦊",
+    keywords: ["fox", "inteligent"]
+  },
+  { 
+    id: "4", 
+    name: "sushi", 
+    type: "fish", 
+    emoji: "🐟", 
+    keywords: ["fishy"] }
+];
 
-function App() {
-  const [searchTerm, setSearchTerm] = useState("");
-
+export default function App() {
+  const [searchTerm, setSearchTerm] = useState("friendly");
   const [searchResults, setSearchResults] = useState([]);
 
+  const handleChange = e => setSearchTerm(e.target.value);
+
   useEffect(() => {
-     const results = Information.filter(data =>
-       data.name.toLowerCase().includes(searchTerm) 
-     );
+    const results = data.filter(o => o.keywords.includes(searchTerm));
 
-     setSearchResults(results);
+    setSearchResults(results);
 
-   }, [searchTerm]);
- 
-   const CountryDetail = props => {
-    const { name, age, country } = props;
-    return (
-      <>
-        <p>{name}</p>
-        <p>{age}</p>
-        <p>{country}</p>
-      </>
-    );
-  };
+  }, [searchTerm]);
 
-   return (
+  return (
     <div className="App">
       <input
         type="text"
-        placeholder="Search"
-        onChange={e => setSearchTerm(e.target.value)}
+        value={searchTerm}
+        onChange={handleChange}
+        placeholder="search"
       />
-      {searchResults.map((name, id) => (
-        <CountryDetail key={id} {...name} />
-      ))}
+      <p>Search by Keyword</p>
+      <p>
+        searchTerm:
+        <br />
+        {searchTerm}
+      </p>
+      <div className="available-data">
+        <h1>Available Data</h1>
+        <p>id: "1",
+    name: "rex",
+    type: "dog",
+    emoji: "🐶",
+    keywords: ["dog", "friendly", "inteligent"]</p>
+
+    <p>
+    id: "2",
+    name: "garfield",
+    type: "cat",
+    emoji: "🐱",
+    keywords: ["cat", "independent"]
+    </p>
+
+    <p>
+    id: "3",
+    name: "foxy",
+    type: "fox",
+    emoji: "🦊",
+    keywords: ["fox", "inteligent"]
+    </p>
+
+    <p>
+    id: "4", 
+    name: "sushi", 
+    type: "fish", 
+    emoji: "🐟", 
+    keywords: ["fishy"] 
+    </p>
+      </div>
+      <div className="results">
+      <h1>Results</h1>
+         <ul>
+          {searchResults &&
+            searchResults.map(item => <ul key={item.id}>
+              <li>{item.name}</li>
+              <li>{item.type}</li>
+              <li>{item.emoji}</li>
+            </ul>)}
+        </ul>
+      </div>
     </div>
   );
- }
-
-export default App;
+}
